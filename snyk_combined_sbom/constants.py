dd6b568cf439289df0368b034b33b2260e825ef3
Troy Havelock <troy.havelock@snyk.io>
2023-06-30 17:01:05 -0400

    Initial Commit

---
+++ snyk_combined_sbom/constants.py
@@ -0,0 +1,7 @@
+"""Collection of contants
+"""
+
+SNYK_REST_API_BASE_URL  = 'https://api.snyk.io/rest'
+SNYK_REST_API_VERSION   = '2023-06-22~beta'
+
+DEFAULT_SBOM_FORMAT     = 'cyclonedx1.4%2Bjson'
